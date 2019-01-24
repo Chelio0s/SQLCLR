91d3b314a15d1fa1dfd3efabf13c3599b7250e59
Chelio0s <trofimov.developer@gmail.com>
2019-01-24 16:06:48 +0300

    инструкция trustworthy

---
+++ DatabaseCLR/DatabaseCLR/Properties/AssemblyInfo.cs
@@ -0,0 +1,23 @@
+﻿using System.Reflection;
+
+// Общими сведениями о сборке управляет следующий 
+// набор атрибутов. Измените значения этих атрибутов, чтобы изменить данные,
+// связанные со сборкой SQLCLR.
+[assembly: AssemblyTitle("SQLCLR для Preactor")]
+[assembly: AssemblyDescription("Сборка для облегчения некоторых задач для SQL")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("LACIT")]
+[assembly: AssemblyProduct("SQLCLRDB")]
+[assembly: AssemblyCopyright("Trofimov ©")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Сведения о версии для сборки состоят из следующих четырех значений:
+//
+//      Основная версия
+//      Дополнительная версия 
+//      Номер построения
+//      Редакция
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
